82cd07493a9bb876766f84a95a501693d2c229c3
NurilMuqit <106947141+NurilMuqit@users.noreply.github.com>
2022-10-10 10:33:16 +0800

    Update and rename database.sql to ddl_database.sql

--- database.sql
+++
@@ -1,8 +0,0 @@
--- CREATE DATABASE praktisi;
-
--- CREATE TABLE student{
--- id INT(10),
--- name VARCHAR(100),
--- address VARCHAR(100),
--- major VARCHAR(100)
--- }
---
+++ ddl_database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE praktisi;
+
+CREATE TABLE student{
+id INT(10),
+full_name VARCHAR(100),
+address VARCHAR(100),
+major VARCHAR(100)
+}
