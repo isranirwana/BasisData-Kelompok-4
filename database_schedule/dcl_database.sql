e616508f21c390809c02f3dc826435e99464987b
Chindy febryan <45525155+chindyfebryan@users.noreply.github.com>
2022-10-10 17:00:06 +0800

    cancel privilege from user

--- database_schedule/dcl_database.sql
+++ database_schedule/dcl_database.sql
@@ -3,3 +3,6 @@ CREATE USER 'klp4'@'localhost' IDENTIFIED BY 'kelompok4';
 
 #memberi privilege CRUD ke user klp4
 GRANT INSERT, UPDATE, DELETE, SELECT ON *.* TO 'klp4'@'localhost';
+
+#cancel privilege INSERT dari user klp4
+REVOKE INSERT ON *.* FROM 'klp4'@'localhost';
