a850d4677332aa08d3046adca40408c007618c3b
Chindy febryan <45525155+chindyfebryan@users.noreply.github.com>
2022-10-10 16:58:04 +0800

    give privileges to user

--- database_schedule/dcl_database.sql
+++ database_schedule/dcl_database.sql
@@ -1,2 +1,5 @@
 #membuat user database
 CREATE USER 'klp4'@'localhost' IDENTIFIED BY 'kelompok4';
+
+#memberi privilege CRUD ke user klp4
+GRANT INSERT, UPDATE, DELETE, SELECT ON *.* TO 'klp4'@'localhost';
