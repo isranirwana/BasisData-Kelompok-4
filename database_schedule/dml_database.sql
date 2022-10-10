b56d9773f427339ca12c7afed3619b0017607f20
isty03 <114085968+isty03@users.noreply.github.com>
2022-10-10 15:11:25 +0800

    Update dml_database.sql

--- database_schedule/dml_database.sql
+++ database_schedule/dml_database.sql
@@ -7,8 +7,18 @@ VALUES (34, 'Muhammad Nuril Muqit', 'Graha Cipta Hertasning', 'Sistem Informasi'
 (44, 'Adelia Puspita Hilal', 'Perumnas Antang', 'Sistem Informasi'),
 (46, 'Putri Aulia Nur Salsabila', 'Jl. Sejati', 'Sistem Informasi');
 
-INSERT INTO course
-VALUES ( );
+INSERT INTO course(course_id,course_name,duration)
+VALUES
+(612, 'Pendidikan Agama', '90'),
+(103, 'Struktur Diskrit', '150'),
+(402, 'Sistem Basis Data I', '100'),
+(502, 'Praktikum Sistem Basis Data I', '50'),
+(602, 'Pemrograman Web', '100'),
+(701, 'Praktikum Pemrograman Web', ' 50'),
+(902, 'Praktikum Analisis dan Perancangan Sistem', '50'),
+(802, 'Analisis dan Perancangan Sistem', '100'),
+(303, 'Struktur Data', '150'),
+(203, 'Riset Teknologi Informasi', '150');
 
 INSERT INTO schedule
 VALUES ( );
