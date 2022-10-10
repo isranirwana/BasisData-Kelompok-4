a62c92505813b54ffaaea8d437a4ac35396cd692
isranirwana <114140658+isranirwana@users.noreply.github.com>
2022-10-10 05:42:32 +0000

    Update dml_database.sql

--- database_schedule/dml_database.sql
+++ database_schedule/dml_database.sql
@@ -1,6 +1,6 @@
 INSERT INTO student
 VALUES (8071211034, 'Muhammad Nuril Muqit', 'Graha Cipta Hertasning', 'Sistem Informasi'),
-(8071211036, 'Isra Nirwana Nur. N Kalau', 'Veteran Utara', 'Sistem Informasi'),
+(8071211036, 'Isra Nirwana Nur. N Kalau', 'Jl. Veteran Utara', 'Sistem Informasi'),
 (8071211038, 'Chindy Febryan Erwana Putri', 'Btn Minasa Upa', 'Sistem Informasi'),
 (8071211039, 'Isty Hamdayani', 'Perintis Kemerdekaan', 'Sistem Informasi'),
 (8071211041, 'Fathur Rachman Alamsyah', 'Bumi Tamalanrea Permai', 'Sistem Informasi'),
