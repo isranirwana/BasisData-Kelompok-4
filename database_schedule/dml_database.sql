c109b1422e48c7268daf0bb63066c74a27bee0da
Chindy febryan <45525155+chindyfebryan@users.noreply.github.com>
2022-10-10 12:51:04 +0800

    Insert data student

--- database_schedule/dml_database.sql
+++ database_schedule/dml_database.sql
@@ -1,5 +1,11 @@
--- INSERT INTO
--- VALUE
+INSERT INTO student
+VALUES (8071211034, 'Muhammad Nuril Muqit', 'Graha Cipta Hertasning', 'Sistem Informasi'),
+(8071211036, 'Isra Nirwana Nur. N Kalau', 'Veteran Utara', 'Sistem Informasi'),
+(8071211038, 'Chindy Febryan Erwana Putri', 'Btn Minasa Upa', 'Sistem Informasi'),
+(8071211039, 'Isty Hamdayani', 'Perintis Kemerdekaan', 'Sistem Informasi'),
+(8071211041, 'Fathur Rachman Alamsyah', 'Bumi Tamalanrea Permai', 'Sistem Informasi'),
+(8071211044, 'Adelia Puspita Hilal', 'Perumnas Antang', 'Sistem Informasi'),
+(8071211046, 'Putri Aulia Nur Salsabila', 'Jl. Sejati', 'Sistem Informasi');
 
 -- UPDATE
 -- SET 
